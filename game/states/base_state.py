42d84cc8c6b58c009fdd480fb00627861e2f363b
DrauFhauen <94233263+DrauFhauen@users.noreply.github.com>
2026-03-05 23:56:34 +0100

    v0.1.0: -cut basestate useles funktionen

--- game/states/base_state.py
+++ game/states/base_state.py
@@ -4,15 +4,5 @@ class BaseState:
     def __init__(self, game_manager):
         self.game_manager = game_manager
 
-    def update(self, dt, mouse_pos):
-        """Wird pro Frame aufgerufen."""
-
     def draw(self, screen):
-        """Zeichnet den State auf den Bildschirm."""
         raise NotImplementedError("draw() muss in State-Klassen implementiert werden")
-
-    def on_mouse_down(self, pos, button):
-        """Optionaler Mausklick-Handler."""
-
-    def on_key_down(self, key, mod=0):
-        """Optionaler Tastatur-Handler."""
